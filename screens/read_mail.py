84cf126fd870ffd4847790950800d01dabdc6c34
mridulmaikhuri <mridulmaikhuri1234@gmail.com>
2026-03-25 21:34:27 +0530

    commit

--- screens/read_mail.py
+++ screens/read_mail.py
@@ -4,20 +4,34 @@ from textual.app import ComposeResult
 from services.read_mail import read_mails
 from screens.view_mail import ViewMail
 from screens.mock_emails import mock_emails
+from textual.containers import VerticalGroup, Vertical, Container, HorizontalScroll
 
 def format_row(no, sender, subject, date):
-    no = no[:10].replace('[', '(').replace(']', ')').ljust(10)
-    sender = sender[:25].replace('[', '(').replace(']', ')').ljust(25)
-    subject = subject[:40].replace('[', '(').replace(']', ')').ljust(40)
-    date = date[:16].replace('[', '(').replace(']', ')').ljust(16)
+    no = no[:4].replace('[', '(').replace(']', ')').ljust(4)
+    sender = sender[:18].replace('[', '(').replace(']', ')').ljust(18)
+    subject = subject[:25].replace('[', '(').replace(']', ')').ljust(25)
+    date = date[:10].replace('[', '(').replace(']', ')').ljust(10)
     return f"{no} | {sender} | {subject} | {date}"
 
-class ReadMail(Screen):
-    CSS_PATH = "read_mail.tcss"
-    BINDINGS = [
-        ("p", "prev", "Back")
-    ]
-    
+class PreviewMail(VerticalGroup):
+    def __init__(self, email):
+        super().__init__()
+        self.email = email
+        
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            Container(
+                Static(f"From   : {self.email['sender']}"),
+                Static(f"Subject: {self.email['subject']}"),
+                Static(f"Date   : {self.email['date']}"),
+                id="metadata"
+            ),
+            Container(
+                Static(self.email['body'])
+            )
+        )
+
+class MailList(VerticalGroup):
     def __init__(self):
         super().__init__()
         self.mails = mock_emails
@@ -33,15 +47,49 @@ class ReadMail(Screen):
                 mail["date"]
             )
             list_view.append(ListItem(Label(row), id=f"email-{i}"))
+            
+    def compose(self) -> ComposeResult:
+        yield Vertical(
+            Static("Inbox\n", id="inbox"),
+            Static(
+                format_row("S.no", "Sender", "Subject", "Date"),
+                id="header_row"
+            ),
+            ListView(id="mail_list")
+        )
+
+class ReadMail(Screen):
+    CSS_PATH = "read_mail.tcss"
+    BINDINGS = [
+        ("p", "prev", "Back")
+    ]
+    
+    def __init__(self):
+        super().__init__()
+        self.mails = mock_emails
+    
+    def on_mount(self):
+        mailList = self.query_one(MailList)
+        self.set_focus(mailList)
+    
+    # def on_mount(self):
+    #     list_view = self.query_one("#mail_list", ListView)
+
+    #     for i, mail in enumerate(self.mails):
+    #         row = format_row(
+    #             str(i + 1),
+    #             mail["sender"],
+    #             mail["subject"],
+    #             mail["date"]
+    #         )
+    #         list_view.append(ListItem(Label(row), id=f"email-{i}"))
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Static("Inbox\n", id="inbox")
-        yield Static(
-            format_row("No", "Sender", "Subject", "Date"),
-            id="header_row"
+        yield HorizontalScroll(
+            MailList(),
+            PreviewMail(self.mails[0]),
         )
-        yield ListView(id="mail_list")
         yield Footer()
         
     def action_prev(self) -> None:
